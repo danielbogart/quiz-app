71272aaaaa7f06b13b55ff7fdbaf774ed8d9b5de
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-06-16 20:56:13 -0700

    Changed a few questions

--- app.js
+++ app.js
@@ -33,14 +33,14 @@ $(document).ready(function() {
 		}
 	}
 
-	var q1 = new newQuestion("What is the name of the FBI agent played by Keanu Reeves?", "Jim", "Johnny", "Jared", "Quintus", "b", "\"My name's Johnny Utah!\"");
-	var q2 = new newQuestion("Who\'s parachute does Utah end up with after switching a few times?", "Grommet\'s", "Bodhi\'s", "Pappas\'s", "His own", "d", "After they pass around parachutes, switching numerous times, Johnny ends up with his own chute again. \"This one\'s set for a neckbreaker!\"");
+	var q1 = new newQuestion("How many meatball subs does Papas ask Utah to order for him?", "Three", "Two", "Five", "One", "b", "\"Utah. Get me two!\". Despite the fact that it's 10:30 AM, Angelo Pappas is so hungry he \"could eat the ass end out of a dead rhino\", and has Utah order him two meatball subs.");
+	var q2 = new newQuestion("Who\'s parachute does Utah end up with after trading several times?", "Grommet\'s", "Bodhi\'s", "Pappas\'s", "His own", "d", "After they pass around parachutes, switching numerous times, Johnny ends up with his own chute again. \"This one\'s set for a neckbreaker!\"");
 	var q3 = new newQuestion("What are the bank robbers called?", "The Dead Presidents", "The Ex-Presidents", "The Presidents", "The Ringaling Brothers", "b", "They are the Ex-Presidents, and as Bodhi/Reagan says \"We\'ve been screwing you for years, a few more seconds shouldn\'t matter!\"");
 	var q4 = new newQuestion("During the intro scene, what score does Agent Utah receive on his shooting test?", "75%", "90%", "10%", "100%", "d", "100% Utah!");
-	var q5 = new newQuestion("Who punches Johnny in the face while he\'s surfing at Latigo Beach?", "Warchild", "Bunker", "Bodhi", "Babbit", "b", "Bunker decks him, but Johnny gets him back later on the beach. \"Okay. I get it. This is where you tell me that locals rule, and that Yuppie insects like me shouldn\'t be surfing the break, right?\"");
+	var q5 = new newQuestion("Who punches Johnny in the face while he\'s surfing at Latigo Beach?", "Warchild", "Bunker", "Bodhi", "Babbit", "b", "Bunker decks him, but Johnny gets his revenge later on the beach. \"Okay. I get it. This is where you tell me that locals rule, and that Yuppie insects like me shouldn\'t be surfing the break, right?\"");
 	var q6 = new newQuestion("What famed football player does Pappas mistake Johnny for when they meet?", "John Elway", "Johnny Manziel", "Johnny Unitas", "John Madden", "c", "Angelo Pappas is less than thrilled to be \"saddled with some quarterback punk, Johnny Unitas or something\".");
 	var q7 = new newQuestion("Where does Agent Utah find Rosie\'s body while on the hunt for Bodhi?", "Baja", "Sumatra", "Los Angeles", "Miami", "a", "Rosie turns up dead in Baja.");
-	var q8 = new newQuestion("After being chewed out for not delivering any new leads to their boss, what interesting news does Utah have for Babbit?", "He bought a new surfboard", "He caught his first tube", "He found a sand dollar", "He was nominated for an academy award", "b", "\"I caught my first tube today. Sir.\"");
+	var q8 = new newQuestion("After being chewed out for not delivering any new leads to their boss, what interesting news does Utah have for Babbit?", "He bought a new surfboard", "He caught his first tube", "He found a sand dollar", "He's nominated for an Oscar", "b", "\"I caught my first tube today. Sir.\"");
 	var q9 = new newQuestion("Where does Agent Utah finally track down Bodhi in the final scene?", "Bell\'s Beach, Australia", "Desert Point, Lombok", "Black\'s Beach, San Diego", "Uluwatu, Bali", "a", "Bodhi is at Bell\'s Beach for the 50 year storm, and asks to catch just one wave before Johnny takes him in.")
 
 	$('#a').click(function() {
